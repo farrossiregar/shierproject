bf0dbe5c348ef632713a7d7d78283eabf5a6b7ea
farrossiregar <farros.jackson@gmail.com>
2020-02-19 09:10:03 +0700

    update shierproject

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,10 +6,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    public function indexMobile(){
+    public function indexMobile(Request $request){
+        echo $request->userAgent(); die();
         return view('index-mobile');
     }
 
