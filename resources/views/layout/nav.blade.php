cafa0d82b8920730e8c39657b3f08f77ed620f26
farrossiregar <farros.jackson@gmail.com>
2021-02-07 14:26:20 +0700

    update menu logo resize nav 10

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -71,7 +71,7 @@
                             <li>
                                 <a href="{{ route('/') }}">
                                     <!-- <div style="background-image: url('{{ asset('image/shierproject-logo-darkgrey-rect-nav.png') }}'); background-position: center; width: 120px; height: auto;"></div> -->
-                                    <img src="{{ asset('image/shierproject-logo-darkgrey-rect-nav.png') }}" alt="" style="width: 120px;">
+                                    <img src="{{ asset('image/shierproject-logo-darkgrey-rect-nav.png') }}" alt="" style="width: 100px;">
                                 </a>
                             </li>
                         </ul>
